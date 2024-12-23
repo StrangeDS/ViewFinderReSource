0b0029aaaf0e772e63dd8f5fb07c4661588a54b7
StrangeDS <github.com/StrangeDS>
2024-12-23 16:49:26 +0800

    步骤子系统

--- Source/ViewFinderCore/Private/VFStepsRecorderWorldSubsystem.cpp
+++ Source/ViewFinderCore/Private/VFStepsRecorderWorldSubsystem.cpp
@@ -1,6 +1,143 @@
 #include "VFStepsRecorderWorldSubsystem.h"
 
+#include "Kismet/GameplayStatics.h"
+
 TStatId UVFStepsRecorderWorldSubsystem::GetStatId() const
 {
     RETURN_QUICK_DECLARE_CYCLE_STAT(UVFStepsRecorderWorldSubsystem, STATGROUP_Tickables);
 }
+
+void UVFStepsRecorderWorldSubsystem::Initialize(FSubsystemCollectionBase &Collection)
+{
+    Super::Initialize(Collection);
+}
+
+void UVFStepsRecorderWorldSubsystem::Deinitialize()
+{
+    Super::Deinitialize();
+}
+
+void UVFStepsRecorderWorldSubsystem::OnWorldBeginPlay(UWorld &InWorld)
+{
+    Super::OnWorldBeginPlay(InWorld);
+}
+
+void UVFStepsRecorderWorldSubsystem::Tick(float DeltaTime)
+{
+    TimeSinceLastTick += bIsRewinding ? DeltaTime * RewindCurFactor : DeltaTime;
+    while (TimeSinceLastTick > TickInterval)
+    {
+        Time += bIsRewinding ? -TickInterval: TickInterval;
+        if (bIsRewinding)
+        {
+            TickBackward(TickInterval);
+        }
+        else
+        {
+            TickForward(TickInterval);
+        }
+        TimeSinceLastTick -= TickInterval;
+        OnTickTime.Broadcast(Time);
+    }
+}
+
+void UVFStepsRecorderWorldSubsystem::TickForward(float DeltaTime)
+{
+    Time = FMath::Clamp(Time, TIME_MIN, TIME_MAX);
+
+    for (const auto &Target : TickTargets)
+    {
+        IVFStepsRecordInterface::Execute_TickForward(Target.GetObject(), Time);
+    }
+}
+
+void UVFStepsRecorderWorldSubsystem::TickBackward(float DeltaTime)
+{
+    if (Time <= TIME_MIN)
+    {
+        Time = TIME_MIN;
+        EndRewinding();
+        return;
+    }
+
+    while (!Infos.IsEmpty())
+    {
+        auto Info = Infos.Last();
+        if (Info.Time < Time)
+            break;
+
+        IVFStepsRecordInterface::Execute_StepBack(Info.Sender, Info);
+        Infos.Pop(false);
+    }
+
+    for (const auto &Target : TickTargets)
+    {
+        IVFStepsRecordInterface::Execute_TickBackward(Target.GetObject(), Time);
+    }
+}
+
+void UVFStepsRecorderWorldSubsystem::SubmitStep(UObject *Sender, FVFStepInfo &Info)
+{
+    Info.Sender = Sender;
+    Info.Time = Time;
+    Infos.Add(Info);
+}
+
+void UVFStepsRecorderWorldSubsystem::RegisterTickable(TScriptInterface<IVFStepsRecordInterface> Target)
+{
+    check(Target.GetObject()->Implements<UVFStepsRecordInterface>());
+    TickTargets.AddUnique(Target);
+}
+
+void UVFStepsRecorderWorldSubsystem::UnregisterTickable(TScriptInterface<IVFStepsRecordInterface> Target)
+{
+    TickTargets.RemoveSwap(Target);
+}
+
+void UVFStepsRecorderWorldSubsystem::StartRewinding()
+{
+    if (bIsRewinding)
+        return;
+
+    TimeSinceLastTick = 0.f;
+    OnStartRewinding.Broadcast(Time);
+    bIsRewinding = true;
+}
+
+void UVFStepsRecorderWorldSubsystem::EndRewinding()
+{
+    if (!bIsRewinding)
+        return;
+
+    TimeSinceLastTick = 0.f;
+    OnEndRewinding.Broadcast(Time);
+    bIsRewinding = false;
+}
+
+void UVFStepsRecorderWorldSubsystem::RewindToLastKey()
+{
+    for (int i = Infos.Num() - 1; i >= 0; i--)
+    {
+        if (Infos[i].bIsKeyFrame)
+        {
+            float TimeSpan = (Time - Infos[i].Time - TickInterval);
+            TimeSpan = FMath::Max(TimeSpan, TIME_MIN);
+            TimeSpan = FMath::Min(TimeSpan, TimeOfRewindToLastKey);
+            float Speed = TimeSpan / TimeOfRewindToLastKey;
+            RewindCurFactor = Speed;
+            StartRewinding();
+            GetWorld()->GetTimerManager().SetTimer(
+                RewindHandle, [this]() {
+                    RewindCurFactor = RewindFactor;
+                    EndRewinding();
+                },
+                TimeOfRewindToLastKey,
+                false);
+            return;
+        }
+    }
+}
+
+const float UVFStepsRecorderWorldSubsystem::TIME_MAX = 1.0e6;
+
+const float UVFStepsRecorderWorldSubsystem::TIME_MIN = 0.f;
--- Source/ViewFinderCore/Public/VFStepsRecorderWorldSubsystem.h
+++ Source/ViewFinderCore/Public/VFStepsRecorderWorldSubsystem.h
@@ -2,14 +2,95 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/WorldSubsystem.h"
+
+#include "VFStepsRecordInterface.h"
+
 #include "VFStepsRecorderWorldSubsystem.generated.h"
 
-// using namespace ViewFinder;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FVFStepRecorderDelegate, float, Time);
 
 UCLASS(Blueprintable, ClassGroup = (ViewFinder))
 class VIEWFINDERCORE_API UVFStepsRecorderWorldSubsystem : public UTickableWorldSubsystem
 {
 	GENERATED_BODY()
-	
+
+public:
 	virtual TStatId GetStatId() const override;
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	FORCEINLINE float GetTime() { return Time; }
+	
+	FORCEINLINE float GetDeltaTime() { return TickInterval; }
+
+	// 无需tick的可以在此提交
+	void SubmitStep(UObject *Sender, FVFStepInfo &Info);
+
+	// 未注册的瞬时Step
+	TArray<FVFStepInfo> Infos;
+
+public:
+	void TickForward(float DeltaTime);
+
+	void TickBackward(float DeltaTime);
+	
+	UPROPERTY(BlueprintAssignable, Category = "ViewFinder")
+	FVFStepRecorderDelegate OnTickTime;
+
+	UFUNCTION(BlueprintCallable, Category = "ViewFinder")
+	void RegisterTickable(TScriptInterface<IVFStepsRecordInterface> Target);
+
+	UFUNCTION(BlueprintCallable, Category = "ViewFinder")
+	void UnregisterTickable(TScriptInterface<IVFStepsRecordInterface> Target);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	TArray<TScriptInterface<IVFStepsRecordInterface>> TickTargets;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "ViewFinder")
+	void StartRewinding();
+	
+	UPROPERTY(BlueprintAssignable, Category = "ViewFinder")
+	FVFStepRecorderDelegate OnStartRewinding;
+
+	UFUNCTION(BlueprintCallable, Category = "ViewFinder")
+	void EndRewinding();
+
+	UPROPERTY(BlueprintAssignable, Category = "ViewFinder")
+	FVFStepRecorderDelegate OnEndRewinding;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	bool bIsRewinding = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	float RewindFactor = 3.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	float RewindCurFactor = RewindFactor;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	float Time = TIME_MIN;
+
+	// 自定义tick时间
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewFinder")
+	float TickInterval = 0.05f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ViewFinder")
+	float TimeSinceLastTick = 0.f;
+	
+    const static float TIME_MAX;
+    const static float TIME_MIN;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "ViewFinder")
+	void RewindToLastKey();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ViewFinder")
+	float TimeOfRewindToLastKey = 3.0f;
+
+	FTimerHandle RewindHandle;
 };
